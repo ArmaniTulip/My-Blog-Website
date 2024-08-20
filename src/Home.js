f5a50a3bb96b3e467bd8f80e69ea8f76ea2f55cc
ArmaniTulip <169069271+ArmaniTulip@users.noreply.github.com>
2024-08-20 16:36:16 -0400

    Added buttons

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
 const Home = () => {
+
+    const handleClick = () => {
+    console.log('Hello, World');
+}
+
+const handleClickAgain = (name) => {
+    console.log ('Hello ' + name );
+}
+ 
+
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={()=> handleClickAgain('Armani')}>Click me again</button>
+            
         </div>
      );
 }
