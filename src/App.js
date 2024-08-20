54ad848554e78676e201c9f794fb0a0aee4e88f0
ArmaniTulip <169069271+ArmaniTulip@users.noreply.github.com>
2024-08-20 15:47:07 -0400

    Added Home and navbar components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  const link = "https://twitch.tv/twoliptv";
-  // const person = {name: 'armani', age: 30}
+  
   return (
     <div className="App">
-      <div className="content">
-      <h1>{title}</h1>
-      <p>This was liked {likes} times!</p>
-      {/* <p>{person}</p> */}
-      <p>{10}</p>
-      <p>{"hello world"}</p>
-      <p>{[1,2,3,4,5]}</p>
-      <p>{Math.random() * 10}</p>
-
-      <a href={link}>Twitch</a>
-      </div>
+      <Navbar/>
+     <div className="Content">
+      <Home/>
+     </div>
     </div>
   );
 }
 
 export default App;
+
