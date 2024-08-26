66584d9f1695f181a239c8e3e73e6b53bc1d6f5e
ArmaniTulip <169069271+ArmaniTulip@users.noreply.github.com>
2024-08-26 16:40:20 -0400

    Created forms for creating a new blog

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,37 @@
+import userEvent from "@testing-library/user-event";
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Armani');
     return (
         <div className="create">
-        <h2>Add a new blog</h2>
+        <h2>Add a New Blog</h2>
+        <form>
+            <label>Blog Title:</label>
+            <input type="text"
+                required 
+                value={title}
+                onChange={(e) => setTitle(e.target.value)} />
+        </form>
+        <form>
+            <label>Blog body:</label>
+            <textarea
+                required 
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+            ></textarea>
+            <label>Blog Author:</label>
+            <select
+            value = {author}
+            onChange={(e)=> setAuthor(e.target.value)}
+            >
+                <option value="Armani">Armani</option>
+                <option value="Marissa">Marissa</option>
+            </select>
+            <button>Add Blog</button>
+        </form>
         </div>
       );
 }
