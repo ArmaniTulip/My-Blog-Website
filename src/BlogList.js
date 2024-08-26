8bfc174c1143bb3987ba60a1dc5c6a4f7d99b2e7
ArmaniTulip <169069271+ArmaniTulip@users.noreply.github.com>
2024-08-26 10:55:28 -0400

    Added Delete button to each of the blogs

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,6 +1,7 @@
 const BlogList = (props) => {
     const blogs = props.blogs;
     const title = props.title;
+    const handleDelete = props.handleDelete;
  
 
     return (
@@ -10,6 +11,7 @@ const BlogList = (props) => {
             <div className="blog-preview" key={blog.id}>
                 <h2>{blog.title}</h2>
                 <p>Written by {blog.author} </p>
+                <button onClick={() => handleDelete(blog.id)}>delete blog</button>
                 </div>
         ))}
         </div>
