b23a72e348a70d1daaa8459ce8a54ad0e1beacdd
ArmaniTulip <169069271+ArmaniTulip@users.noreply.github.com>
2024-08-26 13:13:24 -0400

    Added useFetch Hook

--- src/Home.js
+++ src/Home.js
@@ -1,50 +1,19 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
-
-    const [isPending, setIsPending] = useState(true);
-
-    const [error, setError] = useState(null)
-
-
-
-    useEffect(() => {
-        setTimeout(() =>{
-            fetch('http://localhost:8000/blogs')
-        .then(res => {
-            console.log(res);
-            if(!res.ok){
-                throw Error('Could Not Fetch Data')
-            }
-            return res.json();
-        })
-        .then(data => {
-            setBlogs(data);
-            setIsPending(false);
-            setError(null)
-        })
-        .catch(err => {
-            setIsPending(false);
-            setError(err.message);
-        })
-        }, 1000);
-    }, []);
+    const {data : blogs , isPending, error} = useFetch('http://localhost:8000/blogs');
 
     return(
         <div className="home">
             {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
-            {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === 'Armani' )} title="Armani's Blogs" />}
-            
         </div>
     );
 }
 
    
  
-
-
 export default Home;
